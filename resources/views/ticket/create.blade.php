b37e487a7fd7d4882b9d9bb86b9b97558ea77116
br3mah700 <nyeleti.br3mah@gmail.com>
2026-01-05 16:11:15 +0200

    ticket tests prefinal

--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -6,9 +6,9 @@
 <style>
     #fullscreen-loader {
         position: fixed;
-        top: 10;
+        top: 0;
         left: 0;
-        width: 30%;
+        width: 100%;
         height: 100%;
         background: rgba(255, 255, 255, 0.8);
         backdrop-filter: blur(5px);
