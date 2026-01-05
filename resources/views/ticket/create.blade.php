41bf3c0f352b2f8a1092c3a1369854a42e7b0d80
br3mah700 <nyeleti.br3mah@gmail.com>
2026-01-05 16:15:30 +0200

    ticket tests prefinal

--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -23,7 +23,7 @@
     #fullscreen-loader .content {
         text-align: center;
         background: rgba(255, 255, 255, 0.95);
-        padding: 40px;
+        padding: 5px;
         border-radius: 15px;
         box-shadow: 0 8px 32px rgba(0, 0, 0, 0.2);
         border: 1px solid rgba(255, 255, 255, 0.3);
