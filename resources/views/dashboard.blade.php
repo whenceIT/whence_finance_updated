d00aa994f219350a6888b638501b31a7f5a9b6cc
br3mah700 <nyeleti.br3mah@gmail.com>
2026-02-02 10:57:02 +0200

    commented unsed script blocking profile completer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3403,8 +3403,8 @@ if($branchUser->role){
                     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-                        + minutes + "m " + seconds + "s";
+                    // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+                    //     + minutes + "m " + seconds + "s";
 
                 });
 
