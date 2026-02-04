b66488b88fc6a3f51bbbe8d2bbb74ca71cc28ff2
whenceIT <whenceit@gmail.com>
2026-02-04 15:45:34 +0200

    dormant loans section 1

--- resources/views/menu/admin.blade.php
+++ resources/views/menu/admin.blade.php
@@ -585,6 +585,14 @@
                     <li><a href="{{ url('loan/branch_uncollected') }}"><i class="fa fa-circle-o"></i> Branch uncollected</a></li>
                     @endif
                 </ul>
+
+                     <ul class="treeview-menu">
+
+                    <!-- Branch Uncollected -->
+                    @if(Sentinel::hasAccess('expenses'))
+                    <li><a href="{{ url('loan/dormant_loans') }}"><i class="fa fa-frown-o"></i>Dormant Loans</a></li>
+                    @endif
+                </ul>
             </li>
 
             <!-- ============================================
