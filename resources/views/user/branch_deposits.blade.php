0b453499415b9f70758122ee0e231534ce75e0c8
whenceIT <whenceit@gmail.com>
2026-02-24 10:12:49 +0200

    enhanced deposit section 1

--- resources/views/user/branch_deposits.blade.php
+++ resources/views/user/branch_deposits.blade.php
@@ -183,9 +183,9 @@ $(document).ready(function () {
         }
 
         if (
-            (currentDepositType == 1 && currentDepositAmount < 7000) ||
-            (currentDepositType == 3 && currentDepositAmount < 5000) ||
-            (currentDepositType == 5 && currentDepositAmount < 4500)
+            (currentDepositType == 1 && currentDepositAmount < 10000) ||
+            (currentDepositType == 3 && currentDepositAmount < 10000) ||
+            (currentDepositType == 5 && currentDepositAmount < 10000)
         ) {
             alert('Please enter minimum deposit amount.');
             return;
