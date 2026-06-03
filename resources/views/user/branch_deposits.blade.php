a9a14ee96a1fda189200b20321bb5b2199ff5981
br3mah700 <nyeleti.br3mah@gmail.com>
2026-06-03 09:09:15 +0200

    Stable Deposit Blocker on BMs

--- resources/views/user/branch_deposits.blade.php
+++ resources/views/user/branch_deposits.blade.php
@@ -53,7 +53,7 @@
 </style>
 
 <!-- Test with Anchor House First -->
-@if( Sentinel::getUser()->role->role_id == 4 && $block->office_id == 2) 
+@if( Sentinel::getUser()->role->role_id == 4 && Sentinel::getUser()->office_id == 2) 
     <x-debt-blocker/>
 @endif
 <div class="content-wrapper">
