b02b5a2b4c115e0e25a53d3eea522e2231de01b8
br3mah700 <nyeleti.br3mah@gmail.com>
2026-06-04 16:47:12 +0200

    md despost

--- resources/views/loan/show.blade.php
+++ resources/views/loan/show.blade.php
@@ -11,7 +11,7 @@
     @endphp
 
     <!-- Test with Anchor House First -->
-    @if($block->role->role_id== 4) 
+    @if(false) 
         @if(!$monthlyDepositDone && request()->path() != 'user/branch_deposits')
             <script>window.location.href = '/user/branch_deposits';</script>
         @else
