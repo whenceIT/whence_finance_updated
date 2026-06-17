cb13b33a9288c89251554329256c4e18225d1b94
br3mah700 <nyeleti.br3mah@gmail.com>
2026-06-17 10:17:04 +0200

    removed global scope on users

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,14 +11,14 @@ class User extends EloquentUser
 
 
     //Where ever User's model is queried, Inactive users will be excluded by default. To include them, use the withTrashed() method.
-    protected static function boot()
-    {
-        parent::boot();
+    // protected static function boot()
+    // {
+    //     parent::boot();
 
-        static::addGlobalScope('excludeInactive', function (Builder $builder) {
-            $builder->where('status', '<>', 'Inactive');
-        });
-    }
+    //     static::addGlobalScope('excludeInactive', function (Builder $builder) {
+    //         $builder->where('status', '<>', 'Inactive');
+    //     });
+    // }
 
     protected $fillable = [
         'email',
