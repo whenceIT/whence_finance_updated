64647035961fcd5e2150d82313a40208936a22a4
whenceIT <whenceit@gmail.com>
2026-06-19 16:26:28 +0200

    added essential expenses

---
+++ app/Models/NonEssentialExpenseTypes.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NonEssentialExpenseTypes extends Model
+{
+    protected $table = "non_essential_expense_types";
+
+    public function gl_account_asset()
+    {
+        return $this->hasOne(GlAccount::class, 'id', 'gl_account_asset_id');
+    }
+
+    public function gl_account_expense()
+    {
+        return $this->hasOne(GlAccount::class, 'id', 'gl_account_expense_id');
+    }
+
+    public function gl_account()
+    {
+        return $this->hasOne(GlAccount::class, 'id', 'gl_account_id');
+    }
+
+    public function gl_accounts()
+    {
+        return $this->belongsToMany(GlAccount::class, 'expense_type_gl_account', 'expense_type_id', 'gl_account_id');
+    }
+}
