57fd5fdf5ae22ecfede285bcc021eefd1038bea8
br3mah700 <nyeleti.br3mah@gmail.com>
2026-06-23 16:14:18 +0200

    Loan Controller fix

--- app/Helpers/BlockerHelper.php
+++ app/Helpers/BlockerHelper.php
@@ -84,7 +84,7 @@ class BlockerHelper
                 'message'=> 'Not a BM or LC'
             ];
         }
-        if ( $user->office_id == 18 || $user->office_id == 46 || $user->office_id == 2) {
+        if ( $user->office_id == 18 || $user->office_id == 46) {
             return [
                 'status'=>false,
                 'amount' => 0,
