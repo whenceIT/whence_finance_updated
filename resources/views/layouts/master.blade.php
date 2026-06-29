bd02951a453792d0500bc5395f6cddc3bae4af0e
br3mah700 <nyeleti.br3mah@gmail.com>
2026-06-29 17:06:25 +0200

    unlocked

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1970,7 +1970,7 @@ $office = $userInfo->office;
 
 
     @include('components.ledger_blocker')
-    @if(Sentinel::getUser() && in_array(Sentinel::getUser()->office_id, [8, 7, 49, 54]) && request()->path() != 'user/branch_deposits')
+    @if(Sentinel::getUser() && in_array(Sentinel::getUser()->office_id, [8, 49, 54]) && request()->path() != 'user/branch_deposits')
         @include('components.current_month_deposit_blocker')
     @endif
 
