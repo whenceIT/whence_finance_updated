7607b19c6fa81ead0c30b993f20a944da3d33028
br3mah700 <nyeleti.br3mah@gmail.com>
2026-07-09 23:34:37 +0200

    desposit page | lock system

--- resources/views/branch-deposits/index.blade.php
+++ resources/views/branch-deposits/index.blade.php
@@ -22,12 +22,12 @@
                 <div class="deposit-header-box">
                     <h2 style="margin-top:0; font-weight: 700; font-size: 28px;" id="monthlyDepositsTitle">Monthly Deposits for {{ $selectedMonthForInput ?? $selectedMonth }}</h2>
                     <p class="text-muted" style="margin-bottom:15px;">
-                        Enter deposit for the allowed monthly deposits required for your branch. Please ensure the total amount covers the full or atleast K5,000 partial minimum required deposit for the month. Once you click "Save Deposit", it will be recorded and cannot be reversed. If you are unsure about the required amount, click "This Month Deposit" to view your current month's deposit status.
+                        Enter deposit for the allowed monthly deposits required for your branch. Please ensure the total amount covers the full or atleast K5,000 minimum.
                     </p>
 
                     <p style="color: rgba(255, 17, 41, 0.84); padding:5px; font-weight: 500; background-color:rgba(255, 245, 246, 0.57); border-radius:4px;">
                         <i class="fa fa-info-circle"></i>
-                        For the savings, salaries, and housing sections to unlock make sure to make and record full payments on the mandatory deposit
+                        For the savings, salaries, and housing sections to unlock make sure to make and record full payments on the mandatory deposit which are the setup debt, building deposit, and administration deposit. Once these are fully paid, the other sections will be available for deposits.
                     </p>
                     <hr style="border-top:1px solid #eee; margin:20px 0;">
                     <div style="display:flex; justify-content:space-between; align-items:flex-end; margin-bottom:10px;">
